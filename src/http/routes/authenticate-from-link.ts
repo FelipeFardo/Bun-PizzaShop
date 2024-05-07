44d6edad3f19f89ffba886cbd9e93a075a2a3ea0
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-05-07 16:15:40 -0300

    feat: route logout and derive

--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -7,7 +7,7 @@ import { eq } from 'drizzle-orm'
 
 export const authenticateFromLink = new Elysia().use(auth).get(
   '/auth-links/authenticate',
-  async ({ query, jwt: { sign }, cookie: { auth }, set }) => {
+  async ({ query, signUser, set }) => {
     const { code, redirect } = query
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -33,17 +33,11 @@ export const authenticateFromLink = new Elysia().use(auth).get(
         return eq(fields.managerId, authLinkFromCode.userId)
       },
     })
-    console.log('BBBBBBBBBBB')
 
-    const token = await sign({
+    await signUser({
       sub: authLinkFromCode.userId,
       restaurantId: managedRestaurant?.id,
     })
-
-    auth.value = token
-    auth.httpOnly = true
-    auth.maxAge = 60 * 60 * 24 * 7 // 7 days
-    auth.path = '/'
 
     await db.delete(authLinks).where(eq(authLinks.code, code))
 
