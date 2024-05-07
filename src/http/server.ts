bb1196241439fb22792190699c25cc393bcccfc4
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-05-07 18:29:42 -0300

    feat: send e-mail with Nodemailer

--- src/http/server.ts
+++ src/http/server.ts
@@ -13,6 +13,20 @@ const app = new Elysia()
   .use(authenticateFromLink)
   .use(getProfile)
   .use(getManagedRestaurant)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case 'VALIDATION': {
+        set.status = error.status
+        return error.toResponse()
+      }
+      default: {
+        set.status = 500
+        console.log(error)
+
+        return new Response(null, { status: 500 })
+      }
+    }
+  })
 
 app.listen(3333, () => {
   console.log('HTTP Server running')
