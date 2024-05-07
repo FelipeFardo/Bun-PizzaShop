dc9b04567344be76b4c086471712c1a0969b5f50
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-05-07 01:32:32 -0300

    feat: route authenticate from link

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,7 +3,12 @@ import { Elysia } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+import { authenticateFromLink } from './routes/authenticate-from-link'
+
+const app = new Elysia()
+  .use(registerRestaurant)
+  .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log('HTTP Server running')
