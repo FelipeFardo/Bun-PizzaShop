361cdcbbfdd9549b3de5878232b235d290f4f738
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-05-07 17:01:29 -0300

    feat: route user profile

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,11 +4,13 @@ import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getProfile } from './routes/get-profile'
 
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
+  .use(getProfile)
 
 app.listen(3333, () => {
   console.log('HTTP Server running')
